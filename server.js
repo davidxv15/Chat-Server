3eaaa514bf1188977f4412e10e03b012686c7093
davidxv15 <davidxvaz@gmail.com>
2024-08-03 23:24:32 -0700

    debugging

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const WebSocket = require("ws");
+const WebSocket = require('ws');
 
 const server = new WebSocket.Server({ port: 3000 });
 
-server.on("connection", (socket) => {
-  console.log("Client connected");
+server.on('connection', (socket) => {
+  console.log('Client connected');
 
-  socket.on("message", (message) => {
-    console.log("Received:", message);
+  socket.on('message', (message) => {
+    console.log('Received:', message.toString());
 
     // Broadcast the message to all clients
     server.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(message.toString());
       }
     });
   });
 
-  socket.on("error", (error) => {
-    console.error("WebSocket error:", error);
+  socket.on('error', (error) => {
+    console.error('WebSocket error:', error);
   });
 
-  socket.on("close", () => {
-    console.log("Client disconnected");
+  socket.on('close', () => {
+    console.log('Client disconnected');
   });
 });
 
-console.log("WebSocket server is running on ws://localhost:3000");
+console.log('WebSocket server is running on ws://localhost:3000');
