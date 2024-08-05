f2d8ef7cbe6175266211e7eef2d6da7a62edbe99
davidxv15 <davidxvaz@gmail.com>
2024-08-04 21:05:47 -0700

    User & Auth content

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,35 +1,37 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const router = express.Router();
 
 // Register new user
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
   try {
     const user = await User.create({ username, password });
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(400).json({ message: 'User registration failed', error });
+    res.status(400).json({ message: "User registration failed", error });
   }
 });
 
 // Login user
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
     const user = await User.findOne({ username });
     if (user && (await user.matchPassword(password))) {
-      const token = jwt.sign({ id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+      const token = jwt.sign({ id: user._id }, "your_jwt_secret", {
+        expiresIn: "1h",
+      });
       res.json({ token });
     } else {
-      res.status(401).json({ message: 'Invalid username or password' });
+      res.status(401).json({ message: "Invalid username or password" });
     }
   } catch (error) {
-    res.status(400).json({ message: 'Login failed', error });
+    res.status(400).json({ message: "Login failed", error });
   }
 });
 
