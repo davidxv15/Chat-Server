d1415dddadfb95e8ef374ff3112510baec6e4894
davidxv15 <davidxvaz@gmail.com>
2024-11-02 22:39:35 -0700

    deployment routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { Server } = require("ws"); // my WebSocket lib import
 const jwt = require("jsonwebtoken"); // JWT library
 
-const authRoutes = require("./Routes/authRoutes");
+const authRoutes = require("./Routes/authenticationRoutes");
 
 // const contentRoutes = require("./Routes/content")
 const { protect } = require("./middleware/auth");
