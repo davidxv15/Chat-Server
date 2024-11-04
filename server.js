02ca2e166dff996f2a69e8d1b847dbbd14a0c214
davidxv15 <davidxvaz@gmail.com>
2024-11-04 09:55:35 -0700

    allowedOrigins order update

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const cors = require("cors");
 
 const allowedOrigins = [
   "http://localhost:3003", // Development URL
-  "https://capable-selkie-5113d6.netlify.app", // Netlify 
   "https://sheltered-ocean-88159.herokuapp.com", // Heroku
+  "https://capable-selkie-5113d6.netlify.app", // Netlify 
 ];
 
 app.use(
