f961e7d251959ab81290a9ff753c97ae98b0755e
davidxv15 <davidxvaz@gmail.com>
2025-03-06 03:18:55 -0700

    Fix(server.js): update CORS for front and backend

--- server.js
+++ server.js
@@ -11,7 +11,25 @@ const app = express();
 app.use(express.json());
 
 const cors = require("cors");
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:3003", // Development URL
+  "https://sheltered-ocean-88159.herokuapp.com", // Heroku
+  "https://chat-department.netlify.app",
+  "https://capable-selkie-5113d6.netlify.app", // Netlify 
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
